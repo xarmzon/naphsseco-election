af531dc8127b90bda2681a1c0bd079d208f58844
Adelola Kayode Samson <xarmzon@gmail.com>
2023-08-18 13:01:25 +0000

    update logo

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -6,7 +6,7 @@ import { APP_NAME } from '../../libs/constants'
 const Logo = () => {
   return (
     <Link href="/">
-      <a className="flex items-center space-x-1">
+      <a className="relative flex items-center space-x-1 text-primary">
         <div className="relative h-10 w-10 md:h-12 md:w-12">
           <Image
             src="/images/logo.png"
@@ -15,9 +15,12 @@ const Logo = () => {
             objectFit="contain"
           />
         </div>
-        <h1 className="text-[14px] font-bold text-primary md:text-lg lg:text-2xl">
+        <h1 className="text-[14px] font-bold  md:text-lg lg:text-2xl">
           {APP_NAME}
         </h1>
+        <span className="absolute -top-1 left-10 text-[9px] md:left-12 lg:text-[11px]">
+          (2023)
+        </span>
       </a>
     </Link>
   )
